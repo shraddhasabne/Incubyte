5bc4402bb44998b1168e16524a56e62d220c0093
shraddhasabne <88568076+shraddhasabne@users.noreply.github.com>
2021-10-18 10:57:26 +0530

    test case3

--- Incubyte/string-calculator-starter-master/src/main/java/calculator/StringCalculator.java
+++ Incubyte/string-calculator-starter-master/src/main/java/calculator/StringCalculator.java
@@ -3,26 +3,47 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) 
-    {
-    	int sum = 0;
-    	StringTokenizer st = new StringTokenizer(input,",");
-    	while(st.hasMoreTokens())
-    	{
-    		int a = 0;
-    		String temp = st.nextToken();
-    		try
-    		{
-    			a = Integer.parseInt(temp);
-    		}
-    		catch(Exception e)
-    		{
-    			a = 0;
-    		}
-    		sum = sum + a;
-    	}
-    	
-    	System.out.println(input+"  "+sum);
-        return sum;
-    }
+  {
+	  	int sum = 0;int a = 0;
+	  	int length = input.length();
+	  	String temp = "";
+	  	for(int i=0;i<length;i++)
+	  	{
+	  		char curr = input.charAt(i);
+	  		if(curr==',' || curr=='\n')
+	  		{
+	  			a = 0;
+	  	  		try
+	  	  		{
+	  	  			a = Integer.parseInt(temp);
+	  	  		}
+	  	  		catch(Exception e)
+	  	  		{
+	  	  			a = 0;
+	  	  		}
+	  	  		sum = sum + a;
+	  	  		temp = "";
+	  		}
+	  		else
+	  		{
+	  			temp = temp + curr;
+	  		}
+	  	}
+	  	
+	  	a = 0;
+		try
+		{
+			a = Integer.parseInt(temp);
+		}
+		catch(Exception e)
+		{
+			a = 0;
+		}
+		sum = sum + a;
+		temp = "";
+	  	
+	  	System.out.println(input+"  "+sum);
+	    return sum;
+  }
 
 }
